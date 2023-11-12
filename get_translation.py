72c788bdc942b4b668e92ac220a3765162c3dc8f
SkyEye_FAST <skyeyefast@teahou.se>
2023-11-12 21:22:32 +0800

    Optimization

--- get_translation.py
+++ get_translation.py
@@ -11,7 +11,8 @@ from requests import get
 P = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".")
 
 # 加载配置
-if not os.path.exists(config_path := os.path.join(P, "configuration.toml")):
+config_path = os.path.join(P, "configuration.toml")
+if not os.path.exists(config_path):
     print("\n无法找到配置文件，请将配置文件放置在与此脚本同级的目录下。")
     sys.exit()
 with open(config_path, "rb") as f:
@@ -28,9 +29,22 @@ if V == "latest":
 print(f"选择的版本：{V}\n")
 
 # 读取语言文件
-language_files = ["en_us.json", "zh_cn.json", "zh_hk.json", "zh_tw.json", "lzh.json"]
+language_dict = {
+    "zh_cn": "简体中文（中国大陆）",
+    "zh_hk": "繁體中文（香港特別行政區）",
+    "zh_tw": "繁體中文（台灣）",
+    "lzh": "文言（華夏）",
+}
+language_list = ["zh_cn", "zh_hk", "zh_tw", "lzh"]
+language_files_list = [
+    "en_us.json",
+    "zh_cn.json",
+    "zh_hk.json",
+    "zh_tw.json",
+    "lzh.json",
+]
 language_data = {}
-for file in language_files:
+for file in language_files_list:
     with open(os.path.join(P, config["version_folder"], V, file), "rb") as f:
         language_data[file.split(".", maxsplit=1)[0]] = json.load(f)
 
@@ -44,56 +58,59 @@ if METHOD == 1:
 
     if translation_key in language_data["en_us"]:
         print(f"\n源字符串：{language_data['en_us'][translation_key]}")
-        print(f"简体中文（中国大陆）：{language_data['zh_cn'].get(translation_key, '不存在')}")
-        print(f"繁體中文（香港特別行政區）：{language_data['zh_hk'].get(translation_key, '不存在')}")
-        print(f"繁體中文（台灣）：{language_data['zh_tw'].get(translation_key, '不存在')}")
-        print(f"文言（華夏）：{language_data['lzh'].get(translation_key, '不存在')}\n")
+        for lang in language_list:
+            print(
+                f"{language_dict[lang]}：{language_data[lang].get(translation_key, '不存在')}"
+            )
+        print()
     else:
         print("未找到对应的键名，请检查输入")
 elif METHOD == 2:
     source_str = input("\n源字符串：")
 
-    if query_keys := [k for k, v in language_data["en_us"].items() if v == source_str]:
+    query_keys = [k for k, v in language_data["en_us"].items() if v == source_str]
+    if query_keys:
         for key in query_keys:
             print(f"\n本地化键名：{key}")
-            print(f"简体中文（中国大陆）：{language_data['zh_cn'].get(key, '不存在')}")
-            print(f"繁體中文（香港特別行政區）：{language_data['zh_hk'].get(key, '不存在')}")
-            print(f"繁體中文（台灣）：{language_data['zh_tw'].get(key, '不存在')}")
-            print(f"文言（華夏）：{language_data['lzh'].get(key, '不存在')}\n")
+            for lang in language_list:
+                print(f"{language_dict[lang]}：{language_data[lang].get(key, '不存在')}")
+            print()
     else:
         print("未找到对应的源字符串，请检查输入")
 elif METHOD == 3:
     source_str = input("\n源字符串的一部分（不区分大小写）：")
 
-    if query_keys := [
+    query_keys = [
         k for k, v in language_data["en_us"].items() if source_str.lower() in v.lower()
-    ]:
+    ]
+    if query_keys:
         for key in query_keys:
             print(f"\n本地化键名：{key}")
             print(f"源字符串：{language_data['en_us'][key]}")
-            print(f"简体中文（中国大陆）：{language_data['zh_cn'].get(key, '不存在')}")
-            print(f"繁體中文（香港特別行政區）：{language_data['zh_hk'].get(key, '不存在')}")
-            print(f"繁體中文（台灣）：{language_data['zh_tw'].get(key, '不存在')}")
-            print(f"文言（華夏）：{language_data['lzh'].get(key, '不存在')}\n")
+            for lang in language_list:
+                print(f"{language_dict[lang]}：{language_data[lang].get(key, '不存在')}")
+            print()
     else:
         print("未找到对应的源字符串，请检查输入")
 elif METHOD == 4:
-    print("\n选择语言：\n1. 简体中文（中国大陆）\n2. 繁體中文（香港特別行政區）\n3. 繁體中文（台灣）\n4. 文言（華夏）")
+    print("\n请选择语言：")
+    for index, seq in enumerate(language_list):
+        print(f"{index + 1}. {seq}：{language_dict[seq]}")
     LANGUAGE_NUM = 0
     while LANGUAGE_NUM not in [1, 2, 3, 4]:
         LANGUAGE_NUM = int(input("请输入编号："))
-    chosen_language = ["zh_cn", "zh_hk", "zh_tw", "lzh"][LANGUAGE_NUM - 1]
+    chosen_language = language_list[LANGUAGE_NUM - 1]
     translation = input("\n翻译后字符串的一部分：")
 
-    if query_keys := [
+    query_keys = [
         k for k, v in language_data[chosen_language].items() if translation in v
-    ]:
+    ]
+    if query_keys:
         for key in query_keys:
             print(f"\n本地化键名：{key}")
             print(f"源字符串：{language_data['en_us'][key]}")
-            print(f"简体中文（中国大陆）：{language_data['zh_cn'].get(key, '不存在')}")
-            print(f"繁體中文（香港特別行政區）：{language_data['zh_hk'].get(key, '不存在')}")
-            print(f"繁體中文（台灣）：{language_data['zh_tw'].get(key, '不存在')}")
-            print(f"文言（華夏）：{language_data['lzh'].get(key, '不存在')}\n")
+            for lang in language_list:
+                print(f"{language_dict[lang]}：{language_data[lang].get(key, '不存在')}")
+            print()
     else:
         print("未找到对应的翻译后字符串，请检查输入")
